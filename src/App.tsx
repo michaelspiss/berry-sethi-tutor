5f43d0f1f6380cd76c7baf3e458d5cfc8cb7522a
Michael Spiss <michaelspiss.dev@mailbox.org>
2022-12-01 21:08:24 +0100

    Add appStateStore, display tutor only after regex input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import {AppShell, Center, Header, MantineProvider} from '@mantine/core';
-import {useState} from "react";
+import {AppShell, Center, Header, MantineProvider, Navbar} from '@mantine/core';
 import RegexInputScreen from "@/layout/presentation/screens/RegexInputScreen";
 import mantineThemeOther from "./configuration/mantineThemeOther";
+import useAppStateStore from "@/layout/stores/appStateStore";
+import StepsProgress from "@/layout/presentation/StepsProgress";
 
 
 export default function App() {
-    const [activeStep, setActiveStep] = useState(4);
+    const regex = useAppStateStore((state) => state.regex);
+    const solveStep = useAppStateStore((state) => state.solveStep);
 
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS theme={{
@@ -15,8 +17,13 @@ export default function App() {
                 <Header height={mantineThemeOther.headerHeight} px={"xs"}>
                     <Center style={{height: mantineThemeOther.headerHeight}} inline>Berry Sethi Tutor</Center>
                 </Header>
+            }
+            navbar={
+                !regex ? undefined : <Navbar width={{base: 240}} p={"xs"}>
+                    <StepsProgress activeStep={solveStep} />
+                </Navbar>
             }>
-                <RegexInputScreen />
+                {!regex ? <RegexInputScreen /> : null}
             </AppShell>
         </MantineProvider>
     );
