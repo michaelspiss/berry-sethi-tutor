cee9b24cf3fdf0ed27a9705af1b080e6db173d5d
Michael Spiss <michaelspiss.dev@mailbox.org>
2022-12-26 15:39:41 +0100

    Add visual feedback to verify button

--- src/features/tree_builder/presentation/VerifyTreeButton.tsx
+++ src/features/tree_builder/presentation/VerifyTreeButton.tsx
@@ -2,23 +2,36 @@ import {Button} from "@mantine/core";
 import steps from "@/tree_builder/domain/steps";
 import useAppStateStore from "@/layout/stores/appStateStore";
 import {useReactFlow} from "reactflow";
+import {useState} from "react";
+import {IconBan, IconCheck} from "@tabler/icons";
 
 export default function VerifyTreeButton() {
     const solveStep = useAppStateStore((state) => state.solveStep);
     const reactFlow = useReactFlow();
+    const [state, setState] = useState<"idle"|"error"|"success">("idle");
 
-    return <Button onClick={() => {
+    return <Button w={75} color={state==="success" ? "green" : state === "error" ? "red" : "blue"} onClick={() => {
         const result = steps[solveStep].verifier(reactFlow.getNodes(), reactFlow.getEdges());
         if(!result.errors) {
             steps[solveStep].cleanup(result.nodes, result.edges);
             reactFlow.setNodes(result.nodes);
             reactFlow.setEdges(result.edges);
             useAppStateStore.setState({solveStep: solveStep + 1, verificationErrors: undefined});
-            const to = setTimeout(() => reactFlow.fitView(), 100);
-            return () => clearTimeout(to);
-            // TODO: display positive feedback
+            const fitView = setTimeout(() => reactFlow.fitView(), 100);
+            setState("success");
+            const feedback = setTimeout(() => setState("idle"), 2500);
+            return () => {clearTimeout(fitView); clearTimeout(feedback);}
         } else {
             useAppStateStore.setState({verificationErrors: result.errors});
+            setState("error");
+            const feedback = setTimeout(() => setState("idle"), 2500);
+            return () => clearTimeout(feedback);
         }
-    }}>Verify</Button>
+    }}>
+        {
+            state === "idle" ? "Verify" :
+                state === "success" ? <IconCheck size={16} /> :
+                    state === "error" ? <IconBan size={16} /> : ""
+        }
+    </Button>
 }
