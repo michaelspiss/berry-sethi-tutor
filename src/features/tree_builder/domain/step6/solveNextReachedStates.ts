961060a1af1c9c18deeee9a9a3f62293f15227e4
Michael Spiss <michaelspiss.dev@mailbox.org>
2023-01-14 20:58:46 +0100

    Fix next reached infinite loop

--- src/features/tree_builder/domain/step6/solveNextReachedStates.ts
+++ src/features/tree_builder/domain/step6/solveNextReachedStates.ts
@@ -25,22 +25,24 @@ export default function solveNextReachedStates(nodes: Node[], edges: Edge[]): So
 export function getNextReached(node: Node, nodes: Node[], pathEdges: Edge[]): string[] {
     return pathEdges
         .filter(edge => edge.source === node.id && edge.sourceHandle === "step2r")
-        .map(edge => traverseAndReturnNextReachedTerminals(node, edge, nodes, pathEdges))
+        .map(edge => traverseAndReturnNextReachedTerminals([], edge, nodes, pathEdges))
         .reduce((a, c) => a.concat(c), []);
 }
 
-function traverseAndReturnNextReachedTerminals(startingNode: Node, edge: Edge, nodes: Node[], pathEdges: Edge[]): string[] {
+function traverseAndReturnNextReachedTerminals(visitedHandles: string[], edge: Edge, nodes: Node[], pathEdges: Edge[]): string[] {
     const node = nodes.find(node => node.id === edge.target)!;
     if (node.type === "terminal") {
         return [node.data.terminalIndex]
     }
 
-    if(node.id === startingNode.id) {
+    if(visitedHandles.includes(node.id + "#" + edge.targetHandle)) {
         return [];
     }
 
+    visitedHandles.push(node.id + "#" + edge.sourceHandle);
+
     return pathEdges
         .filter(pathEdge => pathEdge.source === edge.target && pathEdge.sourceHandle === edge.targetHandle)
-        .map(followingEdge => traverseAndReturnNextReachedTerminals(startingNode, followingEdge, nodes, pathEdges))
+        .map(followingEdge => traverseAndReturnNextReachedTerminals(visitedHandles, followingEdge, nodes, pathEdges))
         .reduce((a, c) => [...a, ...c], [])
 }
