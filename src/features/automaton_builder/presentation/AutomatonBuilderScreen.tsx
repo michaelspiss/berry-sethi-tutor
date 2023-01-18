27c2d38bed734e0f6a00e74a541d0a54ea2cd646
Michael Spiss <michaelspiss.dev@mailbox.org>
2023-01-18 20:35:23 +0100

    Remove unused vertical graph display option

--- src/features/automaton_builder/presentation/AutomatonBuilderScreen.tsx
+++ src/features/automaton_builder/presentation/AutomatonBuilderScreen.tsx
@@ -3,7 +3,7 @@ import {createStyles, ScrollArea, TextInput} from "@mantine/core";
 import InteractiveTreeBuilder from "@/tree_builder/presentation/InteractiveTreeBuilder";
 import HintBar from "@/layout/presentation/HintBar";
 import AutomatonPreview from "@/automaton_builder/presentation/AutomatonPreview";
-import {ChangeEventHandler, useState} from "react";
+import {ChangeEventHandler} from "react";
 import useAppStateStore from "@/layout/stores/appStateStore";
 import RegexHighlighter from "@/analyze_regex/presentation/RegexHighlighter";
 import useAutomaton from "@/automaton_builder/domain/useAutomaton";
@@ -28,11 +28,9 @@ const useStyles = createStyles(() => ({
         display: "flex",
         flexGrow: 1,
         position: "relative",
-        "&.stacked": {
-            flexDirection: "column",
-        }
+        flexDirection: "column",
     },
-    automatonDefinition : {
+    automatonDefinition: {
         flexGrow: 1,
         maxWidth: 300,
         boxSizing: "border-box",
@@ -46,14 +44,14 @@ const useStyles = createStyles(() => ({
     }
 }))
 
-const onFinalStatesChange : ChangeEventHandler<HTMLInputElement> = (event) => {
+const onFinalStatesChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     const selection = event.currentTarget.selectionStart;
     const value = event.currentTarget.value.replaceAll(".", "•");
     useAutomaton.setState({finalStates: value});
     event.currentTarget.value = value;
     event.currentTarget.setSelectionRange(selection, selection);
 }
-const onStatesChange : ChangeEventHandler<HTMLInputElement> = (event) => {
+const onStatesChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     const selection = event.currentTarget.selectionStart;
     const value = event.currentTarget.value.replaceAll(".", "•");
     useAutomaton.setState({states: value});
@@ -62,8 +60,7 @@ const onStatesChange : ChangeEventHandler<HTMLInputElement> = (event) => {
 }
 
 export default function AutomatonBuilderScreen() {
-    const {classes, cx, theme} = useStyles();
-    const [displayGraphsStacked, setDisplayGraphsStacked] = useState(true);
+    const {classes, theme} = useStyles();
     const states = useAutomaton(state => state.states);
     const finalStates = useAutomaton(state => state.finalStates);
     const focusTrap = useFocusTrap();
@@ -71,20 +68,23 @@ export default function AutomatonBuilderScreen() {
     return <div className={classes.wrapper} ref={focusTrap}>
         <HintBar/>
         <div className={classes.horizontalWrapper}>
-            <ScrollArea className={classes.automatonDefinition} mah={"calc(100vh - var(--mantine-header-height) - var(--hint-bar-height, 0))"}>
+            <ScrollArea className={classes.automatonDefinition}
+                        mah={"calc(100vh - var(--mantine-header-height) - var(--hint-bar-height, 0))"}>
                 <pre style={{padding: theme.spacing.xs}}>
                     r=<RegexHighlighter regex={useAppStateStore.getState().regex} inline/><br/>
                     A<sub>r</sub>=(Q,Σ,δ,I,F)<br/>
-                    Q={"{"}<TextInput data-autofocus defaultValue={states} onChange={onStatesChange} styles={{withIcon: {paddingLeft: 26}}} icon={<span style={{color: "black", fontSize: 14, }}>•r,</span>}/>{"}"}<br/>
-                    Σ={"{" + [...new Set(useAppStateStore.getState().regexModel?.getTerminals())].filter(t => t !== "ε").sort().join(",")  + "}"}<br/>
+                    Q={"{"}<TextInput data-autofocus defaultValue={states} onChange={onStatesChange}
+                                      styles={{withIcon: {paddingLeft: 26}}}
+                                      icon={<span style={{color: "black", fontSize: 14,}}>•r,</span>}/>{"}"}<br/>
+                    Σ={"{" + [...new Set(useAppStateStore.getState().regexModel?.getTerminals())].filter(t => t !== "ε").sort().join(",") + "}"}<br/>
                     δ={"{"}<TransitionsEditor/>{"}"}<br/>
                     I={"{•r}"}<br/>
                     F={"{"}<TextInput defaultValue={finalStates} onChange={onFinalStatesChange}/>{"}"}
                 </pre>
             </ScrollArea>
-            <div className={cx(classes.graphWrapper, {["stacked"]: displayGraphsStacked})}>
+            <div className={classes.graphWrapper}>
                 <div className={classes.errors}>
-                    <VerificationErrors />
+                    <VerificationErrors/>
                 </div>
                 <InteractiveTreeBuilder/>
                 <AutomatonPreview/>
