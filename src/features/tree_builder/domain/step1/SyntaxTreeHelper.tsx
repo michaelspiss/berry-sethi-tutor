ffd0bb8759593fe4c8b6ab08214de1d2d2c5c3f3
Michael Spiss <michaelspiss.dev@mailbox.org>
2023-01-25 22:50:14 +0100

    Add symbol meanings to step 1 help

--- src/features/tree_builder/domain/step1/SyntaxTreeHelper.tsx
+++ src/features/tree_builder/domain/step1/SyntaxTreeHelper.tsx
@@ -1,14 +1,48 @@
 import StepHelp from "@/tree_builder/presentation/StepHelp";
 import RegexHighlighter from "@/analyze_regex/presentation/RegexHighlighter";
+import {Table} from "@mantine/core";
 
 export default function SyntaxTreeHelper() {
     return <>
         Drag nodes from the bottom toolbar to build the syntax tree.
         <StepHelp>
             The syntax tree is a representation of the original regular expression as data structure tree.
-            Nodes affect all their children, so <RegexHighlighter regex={"a*"} inline/> would be represented as
+            Nodes affect all their children - as operators affect their operands.
+            <RegexHighlighter regex={" a*"} inline/> would be represented as
             operator <code>*</code> with child <code>a</code>.<br/>
-            Please be aware that by lecture convention, all group operators (alteration and concatenation) are binary.
+            <RegexHighlighter regex={"a|b"} inline/> as operator <code>|</code> with children <code>a</code> and <code>b</code>.<br/>
+            Please be aware that by lecture convention, group operators (alteration and concatenation) are binary.<br/>
+            <br/>
+            <Table>
+                <thead>
+                    <tr>
+                        <th style={{width: 70}}>Symbol</th>
+                        <th>Meaning</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><RegexHighlighter regex={"*"} inline/></td>
+                        <td>Repeats the preceding group/terminal zero to unlimited times</td>
+                    </tr>
+                    <tr>
+                        <td><RegexHighlighter regex={"+"} inline/></td>
+                        <td>Repeats the preceding group/terminal one to unlimited times</td>
+                    </tr>
+                    <tr>
+                        <td><RegexHighlighter regex={"?"} inline/></td>
+                        <td>Makes the preceding group/terminal optional</td>
+                    </tr>
+                    <tr>
+                        <td><RegexHighlighter regex={"|"} inline/></td>
+                        <td>Infix alteration operator "or"</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>Implicit infix concatenation operator, displayed as • in the syntax tree</td>
+                    </tr>
+                </tbody>
+            </Table>
         </StepHelp>
     </>
 }
